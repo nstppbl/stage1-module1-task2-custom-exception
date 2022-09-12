ed2ccd22d203d6ccb3132d72f67f4bcb31300977
nstppbl <1nstppbl@gmail.com>
2022-09-12 10:58:58 +0500

    initial commit

---
+++ src/main/java/com/epam/mjc/MyNewException.java
@@ -0,0 +1,8 @@
+package com.epam.mjc;
+
+public class MyNewException extends IllegalArgumentException {
+
+    public MyNewException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,17 +5,23 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws MyNewException {
+    if (studentID > 0 && studentID < 11) {
+      return Student.getValueOf(studentID);
+    } else {
+      throw new MyNewException("Could not find student with ID" + studentID);
+    }
+
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
-
     for (int i = 0; i < IDs.length; i++) {
       Student student = manager.find(IDs[i]);
       System.out.println("Student name " + student.getName());
     }
 
   }
+
+
 }
